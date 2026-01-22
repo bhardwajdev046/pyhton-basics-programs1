f216f24ba971f7d1675c4a57eb131bfb80bf5c6d
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-01-23 04:02:49 +0530

    Add files via upload

---
+++ frog jump.py
@@ -0,0 +1,19 @@
+def fun(idx,arr,dp):
+    if idx==0:
+        return 0
+    if dp[idx]!=-1:
+        return dp[idx]
+    a=fun(idx-1,arr,dp)+abs(arr[idx]-arr[idx-1])
+    if idx>1:
+        b=fun(idx-2,arr,dp)+abs(arr[idx]-arr[idx-2])
+    else:
+        b=float('inf')
+    dp[idx]= min(a,b)
+    return dp[idx]
+
+
+# arr=[30, 20, 50, 10, 40]
+arr=[20, 30, 40, 20]
+idx=3
+dp=[-1]*(idx+1)
+print(fun(idx,arr,dp))
